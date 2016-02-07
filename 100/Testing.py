2469ec5323ed3ef16653cb17fc69018084a2bc09
Tsunamicom <Tsunamicom2k7@gmail.com>
2016-02-07 14:30:51 -0500

    Added some tests, changed num_range test to look for List

--- 100/Testing.py
+++ 100/Testing.py
@@ -1,13 +1,17 @@
 import unittest
 import p100
 
+
 class Test(unittest.TestCase):
+
     def test_num_range(self):
-        self.assertEquals(p100.num_range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        self.assertEquals(p100.num_range(10, 1), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        self.assertListEqual(p100.num_range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        self.assertListEqual(p100.num_range(10, 1), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
     def test_num_seq(self):
-        self.assertTrue(p100.num_seq(5), 6)
+        self.assertEquals(p100.num_seq(5), 6)
+        self.assertEquals(p100.num_seq(50), 25)
+
 
 if __name__ == '__main__':
     unittest.main()
