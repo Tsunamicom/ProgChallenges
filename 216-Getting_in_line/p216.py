a93c1c3b8456c6e89fb4d9a904d6456a115f646c
tsunamicom <tsunamicom2k7@gmail.com>
2016-02-10 00:43:30 -0500

    Imported itertools, fixed function to find unique pts

--- 216-Getting_in_line/p216.py
+++ 216-Getting_in_line/p216.py
@@ -9,17 +9,18 @@
 # display total connection length
 
 import math
-
+import time
+from itertools import combinations as comb
 
 comp_loc = dict()
 comp_loc['c1'] = (1, 4)
 comp_loc['c2'] = (2, 5)
 comp_loc['c3'] = (10, 10)
-#comp_loc['c4'] = (5, 4)
-#comp_loc['c5'] = (12, 15)
-#comp_loc['c6'] = (1, 2)
-#comp_loc['c7'] = (14, 30)
-#comp_loc['c8'] = (16, 31)
+# comp_loc['c4'] = (5, 4)
+# comp_loc['c5'] = (12, 15)
+# comp_loc['c6'] = (1, 2)
+# comp_loc['c7'] = (14, 30)
+# comp_loc['c8'] = (16, 31)
 
 
 def conn_length(loc1, loc2):
@@ -27,41 +28,25 @@ def conn_length(loc1, loc2):
     return math.sqrt((loc2[0]-loc1[0])**2 + (loc2[1]-loc1[1])**2)
 
 
-def unique_combos():
+def possible_combos():
     """
-    Find unique connections given a dict of computers.
-    Return a list of unique combinations.
+    Find unique cnnections given a dict of computers.
+    Return a list of unique combinations
     """
-
-    # Find all possible combinations given a dict of computers
-    complistz = list()
-    for comp1 in comp_loc:
-        for comp2 in comp_loc:
-            complistz.append([comp1, comp2])
-
-
-    # Find unique connections given the possible combinations
-    unique_list = list()
-    sortedlist = list()
-    for i in complistz:
-        for ii in complistz:
-            if sorted(i) not in sortedlist:
-                if i[0] != i[1]:
-                    unique_list.append(sorted(i))
-                    sortedlist.append(i)
-                    sortedlist.append(sorted(i))
-
-    return sorted(unique_list)
+    combinations = comb(comp_loc.keys(), 2)
+    comblist = list()
+    for i in combinations:
+        comblist.append(list(sorted(i)))
+    return comblist
 
 
 def con_w_dist():
     """
     Given unique connection, determine distance between those connections.
-    Return a dictionary of {Computer 1:{Connected Computer:Distance}}
+    Return a dictionary of {Computer: {Connected Computer: Distance}}
     """
-
-    connection_combos = unique_combos()
     connect_master = dict()
+    connection_combos = possible_combos()
     for connection in connection_combos:
 
         # Calculate distances between all valid points
@@ -78,7 +63,20 @@ def con_w_dist():
 
 distances = con_w_dist()
 
-dist_list = list()
 print(distances)
 
+
+# Testing to find minimum and maximum distances
+maxdist = 0
+start = str
+for con in distances:
+    mindist = 999
+    for con2 in distances[con]:
+        l = distances[con][con2]
+        if l >= maxdist:
+            maxdist = l
+        if l <= mindist:
+            mindist = l
+    print(str(mindist) + ' MIN')
+print(str(maxdist) + ' MAX')
 
